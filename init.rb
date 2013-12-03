9fd2cf984b3d334eb2e9a33ed9d04479ad43cb44
bilel-kedidi <bilel.kedidi@gmail.com>
2013-12-03 07:44:35 -0800

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 Redmine::Plugin.register :automatic_reassign do
   name 'Automatic Reassign plugin'
   author 'Bilel Kedidi'
-  description 'this plugin allow to automatic re-assign an issue after updating an issue'
+  description 'this plugin allow to automatic re-assign to the author after updating an issue'
   version '0.0.1'
 end
 
